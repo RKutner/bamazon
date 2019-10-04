9a2f3fc9c5f2400d3a4bf2dac5229a1957f0f81b
Rob Kutner <Rob.Kutner@gmail.com>
2019-10-03 20:47:58 -0700

    items display and inquirer accepts item id. needs to check against inventory and respond

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,23 +1,45 @@
-var mysql = require("mysql");
-var inquirer= require("inquirer")
+const mysql = require("mysql");
+const inquirer = require("inquirer")
 
+const connection = mysql.createConnection({
+    host: "localhost",
 
-var connection = mysql.createConnection({
-  host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your username
+    user: "root",
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "ice_creamDB"
+    // Your password
+    password: "password",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+const showInventory = () => {
+    console.log("Welcome to Bamazon! Here's our inventory as of now.");
+    connection.query("SELECT * FROM products;", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price);
+        }
+    })
+
+}
+
+const shop = () => {
+    inquirer
+        .prompt([{
+            message: "Enter the ID of the item you would like to buy. Format is ID Product Department Price Stock",
+            type: "input",
+            name: "id"
+        }]).then (function(answer){
+            connection.query("")
+        })
+}
+
+connection.connect(function (err) {
+    if (err) throw err;
+    showInventory();
+    shop()
+    connection.end();
 });
